cf9ed098c2380e4993302c4ab05b0832b761a927
andresromani <romaniandres5@gmail.com>
2020-11-25 18:42:51 -0300

    iframe agregado

--- footer.php
+++ footer.php
@@ -25,10 +25,10 @@
             </div>
 
             <div class="ubicacion">
-                <h4>ubicación</h4>
-
                 <div class="mapa">
+                    <h4>ubicación</h4>
 
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3283.98909819782!2d-58.46073968482647!3d-34.60443718045931!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcca015f0fd73b%3A0x3c1997ddfda5b81b!2sAv.%20Juan%20B.%20Justo%20%26%20Av.%20San%20Mart%C3%ADn%2C%20Buenos%20Aires!5e0!3m2!1ses-419!2sar!4v1606339333368!5m2!1ses-419!2sar" frameborder="0" style="border:0;" height="225px" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
                 </div>
             </div>
         </div>
